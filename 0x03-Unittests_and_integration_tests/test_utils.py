2c8d0b6627e353568453ff084bd843a136fc61eb
Ifeoluwa <ijjayeola@gmail.com>
2025-07-17 19:06:15 +0100

    commiting  other files needed for test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 
 from parameterized import parameterized
 import unittest, utils
-
+from unittest.mock import patch, Mock
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([({"a": 1}, ("a",), 1), ({"a": {"b": 2}}, ("a",), {"b": 2}), ({"a": {"b": 2}}, ("a", "b"), 2)])
@@ -15,6 +15,24 @@ class TestAccessNestedMap(unittest.TestCase):
             self.assertEqual(nested_map, path, 1)
             print('KeyError')
 
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mockget):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        mockget.return_value = mock_response
+
+        mock_result = utils.get_json(test_url)
+        mockget.assert_called_once_with(test_url)
+        
+        self.assertEqual(mock_result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
