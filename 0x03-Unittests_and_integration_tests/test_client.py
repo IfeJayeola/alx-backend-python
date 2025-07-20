37c8aa5df8b9f7220542853a418068f722b7e3d7
Ifeoluwa <ijjayeola@gmail.com>
2025-07-20 07:03:16 +0100

    integration test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from unittest.mock import PropertyMock, patch
 from client import GithubOrgClient
+from fixtures import fixtures
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -58,3 +59,43 @@ class TestGithubOrgClient(unittest.TestCase):
         def test_has_license(self, repo, license_key, expected):
             """Test the has_license method."""
             assert GithubOrgClient.has_license(repo, license_key) == expected
+
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @parameterized_class([{
+        "org_payload": "fixures.org_payload",
+        "repos_payload": "fixtures.repos_payload",
+        "expected_repos": "fixtures.expected_repos",
+        "apache2_repos": "fixtures.apache2_repos",
+
+    }])
+    @classmethod
+    def setUp(cls):
+        """Set up the test class with mock data."""
+        cls.patcher = patch('utils.requests.get')
+        cls.mock_get = cls.patcher.start()
+        cls.mock_get.return_value.json = fixtures.TEST_PAYLOAD
+
+        def side_effect(url):
+            """Mock the requests.get method."""
+            if url == "https://api.github.com/orgs/google":
+                cls.mock_get.json.return_value = cls.org_payload
+            elif url == "https://api.github.com/orgs/google/repos":
+                cls.mock_get.json.return_value = cls.repos_payload
+            else:
+                raise ValueError("Unexpected URL")
+            return cls.mock_get
+        cls.mock_get.side_effect = side_effect
+
+    @classmethod
+    def tearDown(cls):
+        """Stop the patcher after tests."""
+        cls.patcher.stop()
+
+    def test_public_repos(self):
+        """Test the public_repos method with integration data."""
+        client = GithubOrgClient("google")
+        repos = client.public_repos()
+        self.assertEqual(repos, self.expected_repos)
+    def test_public_repos_with_license(self, license="apache-2.0"):
+        pass
