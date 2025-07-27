a46bbc6b55eebf6e206debf93094a98d70e20cf0
Ifeoluwa <ijjayeola@gmail.com>
2025-07-27 05:49:42 +0100

    pagination

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,19 +1,21 @@
 from rest_framework import viewsets
 from chats.models import Conversation as ConversationModel, Message as MessageModel
-from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Conversation
-
+from .permissions import IsParticipant
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.response import Response
-
+from rest_framework.decorators import action
+from .pagination import MessagePagination
 # Create your views here
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = ConversationModel.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipant]
+    pagination_class = MessagePagination
 
+    @action(detail=True, methods=['post'], url_path='send-message')
     def get_queryset(self, request):
         user = request.user
         return ConversationModel.objects.filter(participants__in=[user])
@@ -33,14 +35,15 @@ class ConversationViewSet(viewsets.ModelViewSet):
         serializers = MessageSerializer(data=data)
         if serializers.is_valid():
             serializers.save()
-            return Response(serializers.data, status=201)
-        return Response(serializers.errors, status=400)
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = MessageModel.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipant]
+    pagination_class = MessagePagination
 
     def get_queryset(self):
         conversation_id = self.kwargs.get('conversation_pk')
