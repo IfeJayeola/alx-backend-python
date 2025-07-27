6da1dcb39076109007632cd816faa2ec74edc3c5
Ifeoluwa <ijjayeola@gmail.com>
2025-07-27 03:38:38 +0100

    corrections

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -9,22 +9,32 @@ class UserSerializer(serializers.ModelSerializer):
     'password': {'write_only': True}
 }
 
-class ConversationSerializer(serializers.ModelSerializer):
-    participants = serializers.PrimaryKeyRelatedField(
-            many =True,
-            queryset = User.objects.all(),
-            allow_empty = False)
-    messages = serializers.PrimaryKeyRelatedField(
-            many=True,
-            read_only=True
-        )
-    class Meta:
-        model = Conversation
-        fields = ['conversation_id', 'participants', 'created_at']
-
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = ['message_id', 'sender_id', 'conversation', 'message_body', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = serializers.PrimaryKeyRelatedField(
+            many =True,
+            queryset = User.objects.all(),
+            allow_empty = False)
+    messages = MessageSerializer(
+            many=True,
+            read_only=True
+        )
+
+
+    def validate_participants(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("A conversation must have at least two participants.")
+        return value
+    def get_participants_count(self, obj):
+        return len(obj.participants.all())
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages']
 
--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,14 @@
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from django.urls import path, include
 from .views import MessageViewSet, ConversationViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'message', MessageViewSet)
-router.register(r'conversation', ConversationViewSet, basename='conversations')
+router.register(r'conversation', ConversationViewSet, basename='conversation')
+conversation_router = NestedDefaultRouter(router, r'conversation', lookup='conversation')
+conversation_router.register(r'message', MessageViewSet, basename='conversation-messages')
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
 ]
